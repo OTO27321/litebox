46088e66e204d2e4b0322547dd9b5e0ba33bb5cd
Jay Bosamiya <jayb@microsoft.com>
2024-12-26 18:55:54 -0800

    Auto-implementation of convenience trait

--- litebox/src/sync/mod.rs
+++ litebox/src/sync/mod.rs
@@ -22,11 +22,22 @@ pub use rwlock::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 #[cfg(not(feature = "lock_tracing"))]
 /// A convenience name for specific requirements from the platform
 pub trait RawSyncPrimitivesProvider: platform::RawMutexProvider + 'static {}
+#[cfg(not(feature = "lock_tracing"))]
+impl<Platform> RawSyncPrimitivesProvider for Platform where
+    Platform: platform::RawMutexProvider + 'static
+{
+}
 
 #[cfg(feature = "lock_tracing")]
 /// A convenience name for specific requirements from the platform
 pub trait RawSyncPrimitivesProvider:
     platform::RawMutexProvider + platform::TimeProvider + platform::DebugLogProvider + 'static
+{
+}
+#[cfg(feature = "lock_tracing")]
+impl<Platform> RawSyncPrimitivesProvider for Platform where
+    Platform:
+        platform::RawMutexProvider + platform::TimeProvider + platform::DebugLogProvider + 'static
 {
 }
 
