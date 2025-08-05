4599e48e54b82c0c0002f42bec602700dfe92057
Chuqiz <zhangchuqi1999@gmail.com>
2025-08-05 12:59:30 -0700

    Remove `target=x86_64-unknown-linux-musl` when using syscall_rewriter  (#255)
    We can use default gnu as the building target (for syscall_rewriter and rtld.so).

--- litebox_runner_linux_userland/tests/run.rs
+++ litebox_runner_linux_userland/tests/run.rs
@@ -135,9 +135,9 @@ fn test_runner_with_dynamic_lib(
     install_files(tar_dir.join("out"));
 
     #[cfg(target_arch = "x86_64")]
-    let target = "--target=x86_64-unknown-linux-musl";
+    let target = "--target=x86_64-unknown-linux-gnu";
     #[cfg(target_arch = "x86")]
-    let target = "--target=i686-unknown-linux-musl";
+    let target = "--target=i686-unknown-linux-gnu";
 
     // build litebox_runner_linux_userland to get the latest `litebox_rtld_audit.so`
     let output = std::process::Command::new("cargo")
